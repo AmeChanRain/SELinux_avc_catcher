ae9f1058eef4beaeeb73079c485b1140f5f5b469
AmeChan <79135062+AmeChanRain@users.noreply.github.com>
2023-03-29 00:24:23 +0800

    Do not output duplicate avc

--- find_avc.py
+++ find_avc.py
@@ -6,8 +6,13 @@ if len(sys.argv) < 2:
 
 input_filename = sys.argv[1]
 
+output_lines = set()
+
 with open(input_filename, 'r') as input_file:
     with open('out.txt', 'w') as output_file:
         for line in input_file:
             if 'avc: denied' in line:
-                output_file.write(line)
+                denied_text = line.split('avc: denied')[-1].strip()
+                if denied_text not in output_lines:
+                    output_file.write(line)
+                    output_lines.add(denied_text)
