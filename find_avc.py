0a204067af09519ddc0eef048b8104ed1aaeb9af
AmeChan <79135062+AmeChanRain@users.noreply.github.com>
2023-04-03 23:22:53 +0800

    Improve code and add support for UTF-16LE
    PowerShell uses UTF-16LE as default encoder.

--- find_avc.py
+++ find_avc.py
@@ -1,3 +1,4 @@
+import codecs
 import sys
 
 if len(sys.argv) < 2:
@@ -6,13 +7,19 @@ if len(sys.argv) < 2:
 
 input_filename = sys.argv[1]
 
+try:
+    with codecs.open(input_filename, mode="r", encoding="utf-8") as input_file:
+        lines = input_file.readlines()
+except UnicodeDecodeError:
+    with codecs.open(input_filename, mode="r", encoding="utf-16le") as input_file:
+        lines = input_file.readlines()
+
 output_lines = set()
 
-with open(input_filename, 'r') as input_file:
-    with open('out.txt', 'w') as output_file:
-        for line in input_file:
-            if 'avc: denied' in line:
-                denied_text = line.split('avc: denied')[-1].strip()
-                if denied_text not in output_lines:
-                    output_file.write(line)
-                    output_lines.add(denied_text)
+with open('out.txt', mode="w", encoding="utf-8") as output_file:
+    for line in lines:
+        if "avc: denied" in line:
+            denied_text = line.split("avc: denied")[1].strip()
+            if denied_text not in output_lines:
+                output_file.write(line.strip() + "\n")
+                output_lines.add(denied_text)
